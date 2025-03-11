2d70b00cfdc3afe7b03f80c87bd90a35353f5531
andreyhuey <andreyhuey777@gmail.com>
2025-03-12 00:51:12 +0100

    feat: add MongoDB connection with success and error logging

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const app = express();
 let mongoose;
 try {
   mongoose = require("mongoose");
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 } catch (e) {
   console.log(e);
 }
